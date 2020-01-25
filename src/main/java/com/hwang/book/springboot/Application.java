a5f01c99284303131738819b6fdab6b909dfd999
황선아 <tjsdk2769@gmail.com>
2020-01-25 19:49:18 +0900

    feat : controller / dto / helloController test & dto test 작성

---
+++ src/main/java/com/hwang/book/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.hwang.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application { //메인클래스, 내장 WAS 실행
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
