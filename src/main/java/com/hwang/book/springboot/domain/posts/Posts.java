78f8182f518bdddec4083d23b88913ae60326224
황선아 <tjsdk2769@gmail.com>
2020-01-28 23:35:08 +0900

    create PostsRepository

--- src/main/java/com/hwang/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/hwang/book/springboot/domain/posts/Posts.java
@@ -9,8 +9,11 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor //기본 생성자 추가 public Post(){}와 같은 효과
 @Entity
-// @Entity : 테이블과 링크될 클래스임을 나타냄. 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍으로 테이블 이름을 매칭
-// ex) SalesManager.java -> sales_manager table
+/* @Entity : 테이블과 링크될 클래스임을 나타냄. 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍으로 테이블 이름을 매칭
+    ex) SalesManager.java -> sales_manager table
+   Entity class에는 절대 setter를 생성하지 않음. 해당 클래스의 인스턴스 값들이 언제 어디서 변해야하는지
+   코드상으로 명확하게 구분할 수가 없어, 차후에 기능 변경시 복잡해짐
+ */
 public class Posts {
     @Id //해당 테이블의 PK필드를 나타냄
     @GeneratedValue(strategy = GenerationType.IDENTITY) //PK의 생성 규칙을 나타냄
@@ -28,7 +31,8 @@ public class Posts {
 
     private String author;
 
-    @Builder // 해당클래사의 빌더 패턴 클래스를 생성. 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함 
+    @Builder // 해당클래사의 빌더 패턴 클래스를 생성. 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+    // 어느 필드에 어떤 값을 채워야할지 명확하게 인지가능
     public Posts(String title, String content, String author){
         this.title = title;
         this.content = content;
