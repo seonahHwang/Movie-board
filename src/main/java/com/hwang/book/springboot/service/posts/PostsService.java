2ada4bb74f0c17b08ba8fe0b669a6bd4a9e4d24f
황선아 <tjsdk2769@gmail.com>
2020-01-29 22:46:10 +0900

    feat : 게시글 등록 기능

--- src/main/java/com/hwang/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/hwang/book/springboot/service/posts/PostsService.java
@@ -1,4 +1,19 @@
 package com.hwang.book.springboot.service.posts;
 
+import com.hwang.book.springboot.domain.posts.PostsRepository;
+import com.hwang.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor //생성자를 통한 postsRepository 주입
+@Service
 public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
--- src/main/java/com/hwang/book/springboot/web/PostsApiController.java
+++ src/main/java/com/hwang/book/springboot/web/PostsApiController.java
@@ -1,4 +1,19 @@
 package com.hwang.book.springboot.web;
 
+import com.hwang.book.springboot.service.posts.PostsService;
+import com.hwang.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class PostsApiController {
+    private final PostsService postsService;
+
+    @PutMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
 }
