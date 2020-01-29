2396f57d8761c7d5f5f2e1a8de72ddbfbb50fb11
황선아 <tjsdk2769@gmail.com>
2020-01-29 23:13:55 +0900

    feat : 게시글 등록 test

--- src/main/java/com/hwang/book/springboot/web/PostsApiController.java
+++ src/main/java/com/hwang/book/springboot/web/PostsApiController.java
@@ -3,7 +3,7 @@ package com.hwang.book.springboot.web;
 import com.hwang.book.springboot.service.posts.PostsService;
 import com.hwang.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostsApiController {
     private final PostsService postsService;
 
-    @PutMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
