ff8be0e0821a2e4edeec2705c4d349ca3e916a50
황선아 <tjsdk2769@gmail.com>
2020-02-01 00:38:28 +0900

    JPA Auditing test

--- src/test/java/com/hwang/book/springboot/domain/posts/PostRepositoryTest.java
+++ src/test/java/com/hwang/book/springboot/domain/posts/PostRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -49,4 +50,21 @@ public class PostRepositoryTest {
         assertThat(posts.getContent()).isEqualTo(content);
     }
 
+    @Test
+    public void BaseTimeEntity_등록(){
+        //given
+        LocalDateTime now = LocalDateTime.of(2019,6,4,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> all = postsRepository.findAll();
+        System.out.println(">>>>>>>>>createDate="+all.get(0).getCreatedDate()+"modifiedDate="+all.get(0).getModifiedDate());
+        
+        assertThat(all.get(0).getCreatedDate()).isAfter(now);
+        assertThat(all.get(0).getModifiedDate()).isAfter(now);
+    }
 }
