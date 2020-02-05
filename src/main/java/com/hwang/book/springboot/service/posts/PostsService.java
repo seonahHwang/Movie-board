3a386ee5e1aabc47f8393fbda7a539c33827c464
황선아 <tjsdk2769@gmail.com>
2020-02-06 00:01:25 +0900

    fix transactional import

--- src/main/java/com/hwang/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/hwang/book/springboot/service/posts/PostsService.java
@@ -9,7 +9,7 @@ import com.hwang.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional; // ??
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,6 +31,7 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()
                 -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
