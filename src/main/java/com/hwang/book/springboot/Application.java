d8e28639e9aaf04c28aaea29c2fe6a516a35899a
황선아 <tjsdk2769@gmail.com>
2020-03-02 11:32:38 +0900

    try

--- src/main/java/com/hwang/book/springboot/Application.java
+++ src/main/java/com/hwang/book/springboot/Application.java
@@ -3,7 +3,7 @@ package com.hwang.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+//
 @SpringBootApplication
 public class Application { //메인클래스, 내장 WAS 실행
     public static void main(String[] args) {
