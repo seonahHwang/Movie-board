02996fae411930587f9dfdb90973f8115d3cbb9a
황선아 <tjsdk2769@gmail.com>
2020-03-17 11:48:21 +0900

    feat : ec2 서버 profile명 production-> real

--- src/main/java/com/hwang/book/springboot/Application.java
+++ src/main/java/com/hwang/book/springboot/Application.java
@@ -1,15 +1,8 @@
 package com.hwang.book.springboot;
 
-import org.apache.catalina.core.ApplicationContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.Arrays;
-
-//
 @SpringBootApplication
 public class Application { //메인클래스, 내장 WAS 실행
     public static void main(String[] args) {
